e249496f743132a5b9ba4c04706bc10d64839bd5
MatenRehimi <matenrehimi@hotmail.com>
2019-12-01 07:09:28 +0000

    Added navigation bar

--- empact/src/index.js
+++ empact/src/index.js
@@ -4,10 +4,15 @@ import Home from './Components/Home';
 import SearchPage from './Components/SearchPage';
 import BarcodeScanner from './Components/BarcodeScanner';
 import ProductPage from './Components/ProductPage';
-import {Route, BrowserRouter, Switch} from "react-router-dom";
+import {Link,Route, BrowserRouter, Switch} from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
 
 const apolloClient = new ApolloClient({
   uri: window.location.protocol + "//" + window.location.hostname + ":4000/graphql",
@@ -15,18 +20,38 @@ const apolloClient = new ApolloClient({
 
 class Application extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      homePage:false,
+      barcodePage:false,
+      searchPage:false,
+      productPage:false
+    }
+  }
+
   render () {
     return (
-      <ApolloProvider client={apolloClient}>
-        <BrowserRouter basename = {"/"} >
-          <Switch>
-            <Route exact path = {"/"} component = {Home}/>
-            <Route path = {"/SearchPage"} component = {SearchPage}  />
-            <Route path = {"/BarcodeScanner"} component = {BarcodeScanner}  />
-            <Route path = {"/ProductPage"} component = {ProductPage} />
-          </Switch>
-        </BrowserRouter>
-      </ApolloProvider>
+      <div>
+        <ApolloProvider client={apolloClient}>
+          <BrowserRouter basename = {"/"} >
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton edge="start" color="inherit" aria-label="menu">
+              </IconButton>
+              <Button color="inherit" component={Link} to="/">Home</Button>
+              <Button color="inherit" component={Link} to="/BarcodeScanner">Barcode Scanner Page</Button>
+            </Toolbar>
+          </AppBar>
+            <Switch>
+              <Route exact path = {"/"} component = {Home}/>
+              <Route path = {"/SearchPage"} component = {SearchPage}  />
+              <Route path = {"/BarcodeScanner"} component = {BarcodeScanner}  />
+              <Route path = {"/ProductPage"} component = {ProductPage} />
+            </Switch>
+          </BrowserRouter>
+        </ApolloProvider>
+      </div>
     );
   }
 }
