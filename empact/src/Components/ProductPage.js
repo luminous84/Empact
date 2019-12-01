81fb2daf348ec8a8eca82a1dd7d70e8db8c9c8eb
Harris Mirza <mirza.harris@gmail.com>
2019-12-01 07:05:41 +0000

    Added data to ProductPage

--- empact/src/Components/ProductPage.js
+++ empact/src/Components/ProductPage.js
@@ -1,49 +1,77 @@
 import React from 'react';
 import './ProductPage';
 
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
+
 class ProductPage extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      id:"",
-      name:"Avacado",
-      imageURL:"",
-      price:"£10",
-      weight:"5kg",
-      company:"Asda",
-      //environmentalData
-      origin:"Mexico",
-      transport:"Boat",
-      transportCO2:"400g",
-      recyclablePlastic:"50%",
-      nonRecyclablePlastic:"50%",
-      waterConsumption:"10L",
-      energyConsumption:"50j",
-      greenscore:"3"
+    console.log(props);
 
+    this.state = {
+      id: props.location.state.id, 
+      barcode: props.location.state.barcode
     }
     console.log("made product page");
   }
 
-  render() {
+  query(props){
+    const {data, loading, error} = useQuery(
+      gql`
+      {
+        products(${(props.id === undefined) ? "barcode" : "id"}:"${(props.id === undefined) ? props.barcode : props.id }"){
+          id
+          name
+          imageURL
+          price
+          weight
+          company {
+            name,
+            logoURL
+          }
+          environmentalData {
+            origin
+            transport
+            transportCO2
+            recyclablePlastic
+            nonRecyclablePlastic
+            waterConsumption
+            energyConsumption
+            greenScore
+          }
+        }
+      }
+    `);
+
+    if(loading) return <h1>Loading...</h1>;
+    if(error) return <h1>Error :(</h1>;
+
+    let product = data.products[0];
     return (
       <div>
         <ul>
-          <h1>Company: {this.state.company} Name: {this.state.name} </h1>
-          <h2>Price: {this.state.price} Green score: {this.state.greenscore}</h2>
+          <h1>Company: {product.company.name} Name: {product.name} </h1>
+          <h2>Price: {product.price} Green score: {product.environmentalData.greenscore}</h2>
           <p>Picture goes here</p>
-          <li>Weight: {this.state.weight}</li>
-          <li>Origin: {this.state.origin}</li>
-          <li>Transport: {this.state.transport}</li>
-          <li>TransportCO2: {this.state.transportCO2}</li>
-          <li>Recyclable Plastic: {this.state.recyclablePlastic}</li>
-          <li>Non-Recyclable Plastic:{this.state.nonRecyclablePlastic}</li>
-          <li>Water Consumption: {this.state.waterConsumption}</li>
-          <li>Energy Consumption: {this.state.energyConsumption}</li>
+          <li>Weight: {product.weight}</li>
+          <li>Origin: {product.environmentalData.origin}</li>
+          <li>Transport: {product.environmentalData.transport}</li>
+          <li>TransportCO2: {product.environmentalData.transportCO2}</li>
+          <li>Recyclable Plastic: {product.environmentalData.recyclablePlastic}</li>
+          <li>Non-Recyclable Plastic:{product.environmentalData.nonRecyclablePlastic}</li>
+          <li>Water Consumption: {product.environmentalData.waterConsumption}</li>
+          <li>Energy Consumption: {product.environmentalData.energyConsumption}</li>
 
         </ul>
       </div>
+    );
+  }
+
+  render() {
+    return(
+      <this.query id={this.state.id} barcode={this.state.barcode}></this.query>
     );
   }
 }
